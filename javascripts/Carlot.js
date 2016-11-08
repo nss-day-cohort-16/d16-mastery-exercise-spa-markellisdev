822a03be53afa58e9db2353287a8f8b4b3d80cd9
Mark Ellis <markellisdev@gmail.com>
2016-11-08 14:07:37 -0600

    initial setup with grunt

---
+++ javascripts/Carlot.js
@@ -0,0 +1,17 @@
+"use strict";
+/*jshint loopfunc: true */
+
+var CarLot = (function () {
+  var inventory = [];
+
+  return {
+    loadInventory: function (callback) {
+      var inventoryLoader = new XMLHttpRequest();
+
+      inventoryLoader.addEventListener("load", function () {
+
+      });
+    }
+  };
+
+})();
