c6c9eefed54f9c973fb7d89a9e352168b5fc4d85
Mark Ellis <markellisdev@gmail.com>
2016-11-09 11:16:13 -0600

    XML request complete, headed into Bootstrap

--- javascripts/quiz.js
+++ javascripts/quiz.js
@@ -2,12 +2,28 @@
 /*jshint loopfunc: true */
 
 function populatePage (inventory) {
+	console.log("inventory", inventory);
   // Loop over the inventory and populate the page
 
+  for (var i=0; i<inventory.length; i++) {
+  	var car =
+		`<header class="carHeader">
+			<div class="carTitle">${inventory[i].year}<strong>${inventory[i].make}</strong>: ${inventory[i].model}
+			</div>
+		</header>
+		<div class="carDescription">
+			<p class="carInfo">${inventory[i].description}</p>
+		</div>
+		<footer class="carFooter">"Price": ${inventory[i].price}</footer>
+		`;
+
+  }
+
   // Now that the DOM is loaded, establish all the event listeners needed
-  CarLot.activateEvents();
+  // CarLot.activateEvents();
 }
 
 // Load the inventory and send a callback function to be
 // invoked after the process is complete
-CarLot.loadInventory();
+CarLot.loadInventory(populatePage);
+console.log("car", car);
